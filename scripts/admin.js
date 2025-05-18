75a0b7f84ad30266502620fac6d16ac077aa92f2
shehan774690541 <shehan774690541@gmail.com>
2025-05-18 12:29:26 +0530

    All programs in Requests completere

--- scripts/admin.js
+++ scripts/admin.js
@@ -44,30 +44,115 @@ const data = [
     type: "double",
     status: "waiting",
   },
+  {
+    id: 1,
+    name: "My Name",
+    email: "shehan774690541@gmail.com",
+    mobile: "+211313232323",
+    roomNo: 104,
+    checkIn: "2023-05-15 14:00",
+    checkOut: "2023-05-20 12:00",
+    type: "double",
+    status: "regected",
+  },
+  {
+    id: 1,
+    name: "My Name",
+    email: "shehan774690541@gmail.com",
+    mobile: "+211313232323",
+    roomNo: 104,
+    checkIn: "2023-05-15 14:00",
+    checkOut: "2023-05-20 12:00",
+    type: "double",
+    status: "blabla",
+  }
 ];
 
-const approve = (i) =>{
-  alert("approve"+  i)
-}
-
-const regect = (i) =>{
-  alert("regect"+ i)
+const approve = (i, a) => {
+  const data = {
+    id: i,
+    approve: a
+  }
+  postRequest('/guests/approve', data)
+    .then(result => console.log(result))
+    .catch(err => console.warn('POST Error:', err));
 }
 
 const userStatus = (status, i) => {
   if (status == "checkIn") {
     return "<button class='btn btn-sm btn-success'>Check In</button>";
   } else if (status == "checkout") {
-    return "<button class='btn btn-sm btn-secondary'>Check Out</button>";
-  }else if(status == "waiting"){
-    return `<button onclick='approve(${i})' class='btn btn-sm btn-warning'>Approve</button> <button  onclick='regect(${i})' class='btn btn-sm btn-danger'>Regect</button>`;
+    return "<button class='btn btn-sm btn-info'>Check Out</button>";
+  } else if (status == "waiting") {
+    return `<button onclick='approve(${i}, true)' class='btn btn-sm btn-warning'>Approve</button> <button onclick='approve(${i}, false)' class='btn btn-sm btn-danger'>Regect</button>`;
+  } else if (status == "regected") {
+    return "<button class='btn btn-sm btn-danger'>Regected</button>";
   } else {
-    return `<button onclick='approve(${i})' class='btn btn-sm btn-warning'>Approve</button> <button  onclick='regect(${i})' class='btn btn-sm btn-danger'>Regect</button>`;
+    return `<button class='btn btn-sm btn-secondary'>Invalid</button>`;
   }
 };
 
-for (let i = 1; i <= data.length; i++) {
-  const row = `
+const clearGuests = () => {
+  if (guestsTableBody) {
+    guestsTableBody.innerHTML += '';
+  }
+}
+
+const NewGuess = (data) => {
+  // const data = {
+  //   name: "My Name",
+  //   email: "shehan774690541@gmail.com",
+  //   mobile: "+211313232323",
+  //   roomNo: 104,
+  //   checkIn: "2023-05-15 14:00",
+  //   checkOut: "2023-05-20 12:00",
+  //   type: "double",
+  //   status: "waiting"
+  // }
+  postRequest('', data)
+    .then(result => console.log(result))
+    .catch(err => console.warn('POST Error:', err));
+
+  listGuests();
+}
+
+const HomePageRooms = () => {
+  let data = []
+  const rooms = [
+      {
+        roomId: 4,
+        available: "available",
+        price: 150,
+        type: "Double",
+        action: "j",
+      },
+      {
+        roomId: 5,
+        available: "available",
+        price: 150,
+        type: "Double",
+        action: "j",
+      },
+      {
+        roomId: 6,
+        available: "available",
+        price: 150,
+        type: "Double",
+        action: "j",
+      },
+    ];
+  postRequest('', '')
+    .then(result => data = result)
+    .catch(err => console.warn('POST Error:', err));
+
+  return rooms;
+}
+
+const listGuests = () => {
+  clearGuests();
+
+  for (let i = 1; i <= data.length; i++) {
+    const row = `
       <tr>
         <td>${data[i - 1].name}</td>
         <td>${data[i - 1].email}</td>
@@ -80,5 +165,8 @@ for (let i = 1; i <= data.length; i++) {
 
       </tr>
     `;
-  guestsTableBody.innerHTML += row;
+    guestsTableBody.innerHTML += row;
+  }
 }
+
+
